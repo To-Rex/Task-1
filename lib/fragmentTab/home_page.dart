cb9cfeea998cde8f27d39c66aa812346bdee0e4d
Yorvoration <dev.dilshodjon@gmail.com>
2023-02-12 02:33:12 +0500

    task

--- lib/fragmentTab/home_page.dart
+++ lib/fragmentTab/home_page.dart
@@ -47,70 +47,12 @@ class _HomePageState extends State<HomePage> with WidgetsBindingObserver {
     }else if (state == AppLifecycleState.paused) {
       print('paused');
       startTimer();
-    } else if (state == AppLifecycleState.detached) {
-      print('detached');
-      startTimer();
+      _counter++;
     }
   }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Flutter Tutorial Lifecycle'),
-      ),
-      body: Center(
-        child: Text('Counter: $_counter'),
-      ),
-    );
-  }
-}
-/*
-class _HomePageState extends State<HomePage> with SingleTickerProviderStateMixin {
-  int _counter = 1;
-  var _timer;
-  void startTimer() {
-    _timer = Timer.periodic(const Duration(seconds: 2), (timer) {
-        _counter++;
-        print('$_counter');
-    });
-  }
-
-  @override
-  void initState() {
-
-    super.initState();
-    WidgetsBinding.instance.addObserver(this);
-  }
-  @override
-  void dispose() {
-    super.dispose();
-    print('dispose');
-  }
-
-  bool isVisible = true;
-
-  @override
-  void deactivate() {
-    isVisible = !isVisible;
-    if(isVisible){
-      //onResume
-      print('onResume');
-    }else {
-      //onStop
-      print('onStop');
-    }
-    super.deactivate();
-  }
-
-  @override
-  void didChangeAppLifecycleState(AppLifecycleState state) {
-    print('state = $state');
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.white,
       appBar: PreferredSize(
         preferredSize: const Size.fromHeight(0), // here the desired height
         child: AppBar(
@@ -122,4 +64,4 @@ class _HomePageState extends State<HomePage> with SingleTickerProviderStateMixin
       ),
     );
   }
-*/
+}
