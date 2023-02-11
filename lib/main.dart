a2099a28c625ebceab08cb3f472d975e56166e02
Yorvoration <dev.dilshodjon@gmail.com>
2023-02-11 20:21:31 +0500

    task

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,10 @@ import 'dart:async';
 
 import 'package:flutter/material.dart';
 
+import 'fragmentTab/history_page.dart';
+import 'fragmentTab/home_page.dart';
+import 'fragmentTab/settings_page.dart';
+
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
@@ -21,30 +25,55 @@ class MyCounter extends StatefulWidget {
 }
 
 class _MyCounterState extends State<MyCounter> {
-  int _counter = 1;
-  var _timer;
 
-  @override
-  void initState() {
-    _timer = Timer.periodic(const Duration(seconds: 2), (timer) {
-      setState(() {
-        _counter++;
-      });
+  int _selectedIndex = 0;
+  static const List<Widget> _widgetOptions = <Widget>[
+    HomePage(),
+    HistoryPage(),
+    SettingsPage()
+  ];
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
     });
-    super.initState();
-  }
-
-  @override
-  void dispose() {
-    _timer.cancel();
-    super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text('Counter: $_counter'),
+    return Scaffold(
+      resizeToAvoidBottomInset: false,
+      backgroundColor: Colors.white,
+      body: Center(
+        child: _widgetOptions.elementAt(_selectedIndex),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        showSelectedLabels: false,
+        showUnselectedLabels: false,
+        type: BottomNavigationBarType.fixed,
+        backgroundColor: const Color.fromRGBO(245, 245, 245, 1.0),
+        items:  const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            activeIcon: Icon(Icons.home),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.restore),
+            activeIcon: Icon(Icons.restore),
+            label: 'History',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.settings),
+            activeIcon: Icon(Icons.settings),
+            label: 'Profile',
+          ),
+        ],
+        currentIndex: _selectedIndex,
+        elevation: 5,
+        selectedItemColor: Colors.white,
+        selectedIconTheme: const IconThemeData(color: Colors.indigo),
+        onTap: _onItemTapped,
+      ),
     );
-
   }
 }
